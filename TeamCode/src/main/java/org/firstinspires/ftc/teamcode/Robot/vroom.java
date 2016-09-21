d4654179c03b334272eb7f7c985ab8862c3a956f
Kevin <khenriquez168@gmail.com>
2016-09-21 10:12:24 -0400

    motor controllers gonna go vroom vroom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/vroom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/vroom.java
@@ -1,7 +1,18 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
 /**
  * Created by khenr on 9/20/2016.
  */
-public class vroom {
+public class vroom extends OpMode {
+    // motor controller
+    private DcMotorController dc_drive_controller;
+
+    private DcMotor Rightmotor;
+    private DcMotor Leftmotor;
+
+
 }
