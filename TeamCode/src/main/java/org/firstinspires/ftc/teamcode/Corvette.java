79f46f3c04613b79984522792c7e6d5522ab675f
Kevin <khenriquez168@gmail.com>
2016-09-21 12:21:40 -0400

    deleted vroom but i got a corvette

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corvette.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by khenr on 9/21/2016.
+ */
+public class Corvette extends OpMode {
+
+}
