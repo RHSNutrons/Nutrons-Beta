8746d2584a9495a27f961063df12b0ff2432ff7b
Kevin <khenriquez168@gmail.com>
2016-09-21 13:38:20 -0400

    Basic drivetrain done just needs to adjusted to kiwi and registered to be used in app. (i cant push to git now)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corvette.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corvette.java
@@ -1,10 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by khenr on 9/21/2016.
  */
-public class Corvette extends OpMode {
+public abstract class  Corvette extends OpMode {
+    private DcMotorController dc_drive__controller;
+
+    private DcMotor driveRight;
+    private DcMotor driveLeft;
+
+    @Override
+    public void int() {
+        dc_drive__controller = hardwareMap.dcMotorController.get("drive_controller");
+        driveLeft = hardwareMap.dcMotor.get("drive_left");
+        driveRight = hardwareMap.dcMotor.get("drive_right");
+    }
+
+    @Override
+    public void loop() {
+        driveLeft.setPower(gamepad1.left_stick_y);
+        driveRight.setPower(gamepad2.right_stick_y);
+    }
+
 
 }
