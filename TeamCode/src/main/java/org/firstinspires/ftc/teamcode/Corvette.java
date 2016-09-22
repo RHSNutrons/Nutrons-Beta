d6b7083bdc2264fe5ce05abedc4b9628fbdb26bb
meme <khenriquez168@gmail.com>
2016-09-22 10:58:00 -0400

    added a third motor to better fit the hexagon drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corvette.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corvette.java
@@ -8,22 +8,25 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
  * Created by khenr on 9/21/2016.
  */
 public abstract class  Corvette extends OpMode {
-    private DcMotorController dc_drive__controller;
+    public DcMotorController dc_drive__controller;
 
-    private DcMotor driveRight;
-    private DcMotor driveLeft;
+    public DcMotor side1Motor;
+    public DcMotor side2Motor;
+    public DcMotor side3Motor;
 
     @Override
     public void int() {
         dc_drive__controller = hardwareMap.dcMotorController.get("drive_controller");
-        driveLeft = hardwareMap.dcMotor.get("drive_left");
-        driveRight = hardwareMap.dcMotor.get("drive_right");
+        side1Motor = hardwareMap.dcMotor.get("side1");
+        side2Motor = hardwareMap.dcMotor.get("side2");
+        side3Motor = hardwareMap.dcMotor.get("Side3");
     }
 
     @Override
     public void loop() {
-        driveLeft.setPower(gamepad1.left_stick_y);
-        driveRight.setPower(gamepad2.right_stick_y);
+        side1Motor.setPower(2.5);
+        side2Motor.setPower(2.5);
+        side3Motor.setPower(2.5);
     }
 
 
