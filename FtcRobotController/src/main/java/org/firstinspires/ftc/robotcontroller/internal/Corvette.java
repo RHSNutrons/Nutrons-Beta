5398c55ee3f9f692f794b01f48b547f3db38079e
Kevin <khenriquez168@gmail.com>
2016-10-13 10:20:13 -0400

    Math unicorn cant even

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
@@ -1,34 +1,98 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * Created by khenr on 9/21/2016.
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
- public class Corvette extends OpMode{
-    public DcMotorController dc_drive__controller;
 
-    public DcMotor side1Motor;
-    public DcMotor side2Motor;
-    public DcMotor side3Motor;
+@TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@Disabled
+public class Corvette extends OpMode
+{
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
 
+     private DcMotor leftMotor = null;
+     private DcMotor rightMotor = null;
+     private DcMotor backMotor = null;
 
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
     public void init() {
-        dc_drive__controller = hardwareMap.dcMotorController.get("drive_controller");
-        side1Motor = hardwareMap.dcMotor.get("side1");
-        side2Motor = hardwareMap.dcMotor.get("side2");
-        side3Motor = hardwareMap.dcMotor.get("Side3");
+        telemetry.addData("Status", "Initialized");
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+         leftMotor  = hardwareMap.dcMotor.get("left motor");
+         rightMotor = hardwareMap.dcMotor.get("right motor");
+         backMotor  = hardwareMap.dcMotor.get("back motor");
+
+
+        // eg: Set the drive motor directions:
+        // Reverse the motor that runs backwards when connected directly to the battery
+         leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+          rightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+          backMotor.setDirection(DcMotor.Direction.FORWARD);
+
+         telemetry.addData("Status", "Initialized");
     }
 
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        backMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
     @Override
     public void loop() {
-        side1Motor.setPower(2.5);
-        side2Motor.setPower(2.5);
-        side3Motor.setPower(2.5);
+        telemetry.addData("Status", "Running: " + runtime.toString());
+
+        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+         leftMotor.setPower(-gamepad1.left_stick_y);
+         rightMotor.setPower(-gamepad1.right_stick_y);
+         backMotor.setPower(-gamepad1.left_trigger);
     }
 
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
 
 }
