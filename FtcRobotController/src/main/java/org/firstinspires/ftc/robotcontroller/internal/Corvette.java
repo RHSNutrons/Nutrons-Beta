9adfdc5e8789c726582c8d890f82bb90def33875
Kevin <khenriquez168@gmail.com>
2016-11-16 10:20:14 -0500

    Robot has ice in its viens now

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
@@ -13,17 +13,21 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
     public DcMotor rightMotor;
     public DcMotor leftMotor;
     public DcMotor backMotor;
+    public DcMotor D_angello;
+    public DcMotor inTake;
 
 
     @Override
     public void init() {
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
-        leftMotor = hardwareMap.dcMotor.get("leftMotor");
-        backMotor = hardwareMap.dcMotor.get("backMotor");
+        //leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        //backMotor = hardwareMap.dcMotor.get("backMotor");
+        D_angello = hardwareMap.dcMotor.get("ice in my veins");
+        //inTake = hardwareMap.dcMotor.get("Intake");
     }
 
     @Override
-    public void loop() {
+    public void loop(){
         double throttle = gamepad1.left_stick_y;
         double direction = gamepad1.right_stick_x;
 
@@ -32,9 +36,33 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
         double back = Math.sin(270)*direction;
 
         rightMotor.setPower(3*right);
-        leftMotor.setPower(3*left);
-        backMotor.setPower(3*back);
+        //leftMotor.setPower(3*left);
+        //backMotor.setPower(3*back);
+
+        if (gamepad1.b) {
+            D_angello.setPower(.25); // 25 % motor speed
+
+        }
+        if (gamepad1.a){
+            D_angello.setPower(.5); // 50 % motor speed
+        }
+        if (gamepad1.x) {
+            D_angello.setPower(.75); // 75 % motor speed
+        }
+        if (gamepad1.y) {
+            D_angello.setPower(1); // 100 % motor speed
+        }
+        if (gamepad1.right_bumper) {
+            D_angello.setPower(0);
+        }
+
+
+        // intake
+        if (gamepad2.right_trigger == 1){
+            //inTake.setPower(1.0);
+        }
     }
+
 
     @Override
     public void stop() {
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
@@ -70,6 +70,7 @@ public class   FtcOpModeRegister implements OpModeRegister {
          */
         BlocksOpMode.registerAll(manager);
         manager.register("Corvette", Corvette.class);
+
         /**
          * Register OpModes that use the annotation-based registration mechanism.
          */
