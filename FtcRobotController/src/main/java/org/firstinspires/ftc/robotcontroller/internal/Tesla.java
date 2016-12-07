ff99b8fd94fa4075af3aed08f44817d162f82afe
Kevin <khenriquez168@gmail.com>
2016-12-07 10:11:43 -0500

    auto maybe works?

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tesla.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tesla.java
@@ -5,39 +5,37 @@ package org.firstinspires.ftc.robotcontroller.internal;
  */
 
 public class Tesla extends Corvette {
-    public void main() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        backMotor = hardwareMap.dcMotor.get("backMotor");
+        D_angello = hardwareMap.dcMotor.get("ice in my veins");
+    }
 
-        // drive forward
-        leftMotor.setPower(1);
-        rightMotor.setPower(1);
-        Thread.sleep(3000);
-
-        // turn left
-        backMotor.setPower(-1);
-        leftMotor.setPower(-1);
-        rightMotor.setPower(-1);
-        Thread.sleep(3000);
-
-        // line up
-        backMotor.setPower(0);
-        leftMotor.setPower(1);
-        rightMotor.setPower(1);
-        Thread.sleep(3000);
-
-        // shoot
-
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-        D_angello.setPower(1);
-
-        // tip the capball
-        D_angello.setPower(0);
-        leftMotor.setPower(1);
-        rightMotor.setPower(1);
-
-        // stop
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+    public void run() throws  InterruptedException{
+        Driveforward(1,1000);
+        Turnleft(1, 1000);
+        Driveforward(1,1000);
+        Shoot(1,300);
+        Driveforward(1,1000);
+    }
+    @Override
+    public void stop(){
 
     }
+    // self explanatory
+    public void Driveforward(int Power, int Time) throws InterruptedException{
+        leftMotor.setPower(Power);
+        rightMotor.setPower(Power);
+        Thread.sleep(Time);
+    }
+    public void Turnleft(int Power, int Time) throws  InterruptedException{
+        backMotor.setPower(Power);
+        Thread.sleep(Time);
+    }
+    public void Shoot(int Power, int Time) throws  InterruptedException{
+        D_angello.setPower(Power);
+        Thread.sleep(Time);
+    }
+
 }
