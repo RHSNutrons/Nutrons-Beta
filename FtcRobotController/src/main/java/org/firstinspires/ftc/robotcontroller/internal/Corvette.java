2d209599d52ad45a5fae0bba958f17f91221a282
man <sytect@hotmail.com>
2017-01-04 09:50:12 -0500

    I want a brand new whip (skrt skrt) I want a brand new house

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Corvette.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by khenr & sytect on 9/21/2016.
  */
- public class Corvette extends OpMode{
+
+
+ public class Corvette extends LinearOpMode {
     public DcMotorController dc_drive_controller;
 
     public DcMotor rightMotor;
@@ -16,63 +19,94 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
     public DcMotor D_angello;
     public DcMotor inTake;
     public DcMotor inTake2;
+    public Servo ramp;
+    public Servo ramp2;
 
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialization");
+        telemetry.update();
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         backMotor = hardwareMap.dcMotor.get("backMotor");
-        D_angello = hardwareMap.dcMotor.get("ice in my veins");
+        /*D_angello = hardwareMap.dcMotor.get("shooter");
         inTake = hardwareMap.dcMotor.get("Intake");
-        inTake = hardwareMap.dcMotor.get("Intake2");
+        inTake2 = hardwareMap.dcMotor.get("Intake2");
+        ramp = hardwareMap.servo.get("ramp");
+        ramp2 = hardwareMap.servo.get("second ramp");*/
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // telementary my dear watson
+            double throttle = gamepad1.left_stick_y; // throttle
+            double direction = gamepad1.right_stick_x; // direction
+
+            /*
+            if(throttle>=0.2&&direction>=0.2){
+                throttle=1;
+                direction=1;
+            }else if(throttle>=0.2&&direction<=-0.2){
+                throttle=1;
+                direction=-1;
+            }else if(throttle<=-0.2&&direction>=0.2){
+                throttle=-1;
+                direction=1;
+            }else if(throttle<=-0.2&&direction<=-0.2){
+                throttle=-1;
+                direction=-1;
+            }
+            */
+
+            double left = Math.cos(150) * direction + Math.sin(150) * throttle;
+            double right = Math.cos(30) * direction - Math.sin(30) * throttle;
+            double back = direction;
+
+            rightMotor.setPower(right);
+            leftMotor.setPower(left);
+            backMotor.setPower(back);
+
+            /*
+            if (gamepad2.b) {
+                D_angello.setPower(-.25); // 25 % motor speed
+
+            }
+            if (gamepad2.a) {
+                D_angello.setPower(-.5); // 50 % motor speed
+            }
+            if (gamepad2.x) {
+                D_angello.setPower(-.75); // 75 % motor speed
+            }
+            if (gamepad2.y) {
+                D_angello.setPower(-1); // 100 % motor speed
+            }
+            if (gamepad2.dpad_down) { // stop
+                D_angello.setPower(-0);
+            }
+
+
+            //intake
+            if (gamepad2.left_trigger == 1) {
+                inTake.setPower(-1.0);
+                inTake2.setPower(-1.0);
+            }
+            if (gamepad2.right_trigger == 1) {
+                inTake.setPower(0);
+                inTake2.setPower(0);
+            }
+            if (gamepad2.dpad_up) {
+                ramp.setPosition(1);
+                ramp2.setPosition(1);
+            }
+            if (gamepad2.dpad_left) {
+                ramp.setPosition(0);
+                ramp2.setPosition(0);
+            }*/
+
+            idle();
+        }
     }
-
-    @Override
-    public void loop(){
-        double throttle = gamepad1.left_stick_y;
-        double direction = gamepad1.right_stick_x;
-
-        double right = Math.sin(150)*direction - Math.cos(150)*throttle;
-        double left = Math.sin(30)*direction + Math.cos(30)*throttle;
-        double back = Math.sin(270)*direction;
-
-        rightMotor.setPower(3*right);
-        leftMotor.setPower(3*left);
-        backMotor.setPower(3*back);
-
-        if (gamepad2.b) {
-            D_angello.setPower(.25); // 25 % motor speed
-
-        }
-        if (gamepad2.a){
-            D_angello.setPower(.5); // 50 % motor speed
-        }
-        if (gamepad2.x) {
-            D_angello.setPower(.75); // 75 % motor speed
-        }
-        if (gamepad2.y) {
-            D_angello.setPower(1); // 100 % motor speed
-        }
-        if (gamepad2.right_bumper) {
-            D_angello.setPower(0);
-        }
-
-
-        // intake
-        if (gamepad2.left_trigger == 1){
-            inTake.setPower(1.0);
-        }
-        if (gamepad2.right_trigger == 1){
-            inTake.setPower(1.0);
-        }
-    }
-
-
-    @Override
-    public void stop() {
-
-    }
-
-
 }
+
+
